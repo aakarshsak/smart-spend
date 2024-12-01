a36497642317fde10fca300c967d2a2eaf6bc59c
Aakarsh Sinha <aakarshsak@gmail.com>
2024-12-01 20:01:38 +0530

    Adding fresh expense management

---
+++ backend/expense-management/src/main/java/com/zoro/smart_spend/expense_management/ExpenseController.java
@@ -0,0 +1,16 @@
+package com.zoro.smart_spend.expense_management;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/expenses")
+public class ExpenseController {
+
+    @GetMapping("")
+    public String hello() {
+        return "HELLO WORLD";
+    }
+}
